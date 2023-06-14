704a7d8372e7fd6cfedbc6a23764128b17bb6fbb
Jian <wood.azure@gmail.com>
2023-06-14 18:44:09 +1200

    added SWR test

--- app/posts/first-post/page.tsx
+++ app/posts/first-post/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from "next/head";
+
+import useSWR from 'swr';
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const YourComponent = () => (
   <Image
@@ -13,13 +18,28 @@ const YourComponent = () => (
 );
 
 export default function FirstPost() {
+  const { data, error, isLoading } = useSWR('https://rickandmortyapi.com/api/character/?page=19', fetcher);
+
+  if (error)
     return (
-      <>
-        <h1>First Post</h1>
-        <YourComponent />
-        <h2>
-          <Link href="/">Back to home</Link>
-        </h2>
-      </>
+      <h2>{error}</h2>
+
     );
-  }
+
+  return (
+    <>
+      <h1>First Post</h1>
+      <YourComponent />
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+
+
+      <p>{isLoading ? "Loading..." : <p>
+        {data.results.map((character: any) => {
+          return <p key={character.id}>{character.name}</p>
+        })}
+      </p>}</p>
+    </>
+  );
+}
